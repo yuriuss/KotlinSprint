563e17bd58343513d1a1aaec0f226cb86853e772
Yury Sliznikov <ysliznikov@gmail.com>
2024-08-21 17:48:04 +0300

    KS-1-3 Выводим год и время первого полёта Юрия Гагарина в космос

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -5,5 +5,16 @@ package lesson_1
  */
 fun main() {
     val year = 1964
+    var hour = "09"
+    var minute = "07"
 
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+    print("$hour:") // можно так
+    println(minute)
+    println("$hour:$minute") // или так
 }
