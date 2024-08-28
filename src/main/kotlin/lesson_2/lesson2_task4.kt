7f38a7c5985da5e4089df04c5a648c36c66d5560
Yury Sliznikov <ysliznikov@gmail.com>
2024-08-28 14:06:38 +0300

    KS-2-4 Исправление недочётов

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,14 +1,13 @@
 package lesson_2
 
-const val BUFF_IN_PERCENTS = 20
-
 fun main() {
+    val buffInPercentage = .2
     val crystalOre = 7
     val ironOre = 11
 
-    val crystalOreWithBuff = crystalOre + (7 * BUFF_IN_PERCENTS) / 100
-    val ironOreWithBuff = ironOre + (ironOre * BUFF_IN_PERCENTS) / 100
+    val bonusCrystalOre = crystalOre * buffInPercentage
+    val bonusIronOre = ironOre * buffInPercentage
 
-    println("$crystalOreWithBuff crystal ore was mined (+$BUFF_IN_PERCENTS%)")
-    println("$ironOreWithBuff iron ore was mined (+$BUFF_IN_PERCENTS%)")
+    println("Бонусная кристаллическая руда: $bonusCrystalOre")
+    println("Бонусная железная руда: $bonusIronOre")
 }
