af39961fc02efc1ad923056b09579aeb266854fa
Yury Sliznikov <ysliznikov@gmail.com>
2024-08-30 18:09:39 +0300

    KS-3-2 Имя и фамилия в гос приложении

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,14 @@
+package lesson_3
+
+fun main() {
+    var lastName = "Андреева"
+    var firstName = "Татьяна"
+    var middleName = "Сергеевна"
+    var age = 20
+
+    println("[$lastName $firstName $middleName, $age]")
+
+    lastName = "Сидорова"
+    age = 22
+    println("[$lastName $firstName $middleName, $age]")
+}
