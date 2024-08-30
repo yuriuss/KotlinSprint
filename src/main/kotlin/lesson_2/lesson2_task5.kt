1ab5901540d70ff985e28b12f96d58598ea20e00
Yury Sliznikov <ysliznikov@gmail.com>
2024-08-30 11:00:22 +0300

    KS-2-5 программа-калькулятор для банковского приложения

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -6,7 +6,7 @@ fun main() {
     var clientSum = 70_000
     var interestRate = 16.7
     var depositeTime = 20
-    val finalAmount: Double = clientSum * (1 + (interestRate / 100)).pow(depositeTime)
+    val finalAmount = clientSum * (1 + (interestRate / 100)).pow(depositeTime)
 
     println("%.3f".format(finalAmount))
 }
