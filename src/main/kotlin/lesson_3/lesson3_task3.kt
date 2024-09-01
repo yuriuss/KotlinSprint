75bd7103d73777cc4599a7a555af316c7678b95b
Yury Sliznikov <ysliznikov@gmail.com>
2024-09-01 10:10:07 +0300

    KS-3-3 Использование raw строк

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -2,13 +2,15 @@ package lesson_3
 
 fun main() {
     var multiNumber = 6
-    println("$multiNumber x 1 = ${multiNumber * 1}\n" +
-            "$multiNumber x 2 = ${multiNumber * 2}\n" +
-            "$multiNumber x 3 = ${multiNumber * 3}\n" +
-            "$multiNumber x 4 = ${multiNumber * 4}\n" +
-            "$multiNumber x 5 = ${multiNumber * 5}\n" +
-            "$multiNumber x 6 = ${multiNumber * 6}\n" +
-            "$multiNumber x 7 = ${multiNumber * 7}\n" +
-            "$multiNumber x 8 = ${multiNumber * 8}\n" +
-            "$multiNumber x 9 = ${multiNumber * 9}")
+    println("""
+        $multiNumber x 1 = ${multiNumber * 1}
+        $multiNumber x 2 = ${multiNumber * 2}
+        $multiNumber x 3 = ${multiNumber * 3}
+        $multiNumber x 4 = ${multiNumber * 4}
+        $multiNumber x 5 = ${multiNumber * 5}
+        $multiNumber x 6 = ${multiNumber * 6}
+        $multiNumber x 7 = ${multiNumber * 7}
+        $multiNumber x 8 = ${multiNumber * 8}
+        $multiNumber x 9 = ${multiNumber * 9}
+    """.trimIndent())
 }
